d99f3c70880e8bded4522dd07bd21f80b4453385
Tomas <tomascorvalan14@gmail.com>
2024-08-02 17:38:35 -0400

    imagenes para los iconos, falta colocar en sus debidas secciones

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -2,13 +2,14 @@ import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 
 function CardPizza({ name, price, ingredients, picture }) {
+  const trozo ="../../public/PizzaTrozo.png"
   return (
     <div className="CardPizza d-flex flex-row mb-3">
       <Card style={{ width: "18rem" }}>
         <Card.Img variant="top" src={picture} />
         <Card.Body>
           <Card.Title>{name}</Card.Title>
-          <Card.Text>Ingredientes: {ingredients}</Card.Text>
+          <Card.Text><img src={trozo}></img>Ingredientes: {ingredients}</Card.Text>
         </Card.Body>
         <ListGroup className="list-group-flush">
           <ListGroup.Item>Valor:{price}</ListGroup.Item>
