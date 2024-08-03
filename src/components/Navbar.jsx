3a2eb970d5f73610b1407054c7e1b147d4a50e78
Tomas <tomascorvalan14@gmail.com>
2024-08-03 15:07:26 -0400

    cambios menores en barra

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+
 function NavBar() {
   return (
     <Navbar bg="dark" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Brand>Pizzeria Mamma Mia!</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link href="#home">🍕Home</Nav.Link>
+          <Nav.Link href="#features">🔒Login</Nav.Link>
+          <Nav.Link href="#pricing"  >🔏Register</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
