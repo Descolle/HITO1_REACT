796b3c08e0245f4fdf671a03bb156e793bc119ce
Tomas <tomascorvalan14@gmail.com>
2024-08-03 20:26:11 -0400

    arreglado botones, aun falta el valor de las pizzas y arreglar texto

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,27 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+import lockopen  from "../assets/img/lockOpen.png"
+import lock from "../assets/img/lock.png"
 
 function NavBar() {
+  const token = false;
+
   return (
     <Navbar bg="dark" data-bs-theme="dark">
       <Container>
         <Navbar.Brand>Pizzeria Mamma Mia!</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#home">🍕Home</Nav.Link>
-          <Nav.Link href="#features">🔒Login</Nav.Link>
-          <Nav.Link href="#pricing"  >🔏Register</Nav.Link>
+        <Button variant="outline-light" className="text-white">🍕Home</Button>
+          <Button variant="outline-light" className="text-white">
+            <image src={token ? lockopen : lock}></image>
+            {token ? "Profile" : "Register"}
+          </Button>
+          <Button variant="outline-light" className="text-white">
+            <image src={token ? lockopen : lock}></image>
+            {token ? "LogOut" : "Login"}
+          </Button>
         </Nav>
       </Container>
     </Navbar>
